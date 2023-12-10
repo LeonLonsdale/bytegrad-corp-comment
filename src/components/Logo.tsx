808cd80669aefbfdd05f519ecbf49d7ccdfe5c6d
LeonLonsdale <leon.coding@icloud.com>
2023-12-10 14:17:49 +0000

    logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,13 @@
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <a href='/'>
+      <img
+        src='https://bytegrad.com/course-assets/js/1/logo.svg'
+        alt='logo'
+        className='logo'
+      />
+    </a>
+  );
 };
 
 export default Logo;
