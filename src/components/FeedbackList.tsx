c773e841b7af6e00d047d6dc6032b9ea72a56739
LeonLonsdale <leon.coding@icloud.com>
2023-12-10 14:37:12 +0000

    add redix ui icons, FeedbackList component

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,7 +1,32 @@
+import { TriangleUpIcon } from '@radix-ui/react-icons';
+
 type FeedbackListProps = {};
 
 const FeedbackList = ({}: FeedbackListProps) => {
-  return <ol className='feedback-list'>FeedbackList</ol>;
+  return (
+    <ol className='feedback-list'>
+      <li className='feedback'>
+        <button>
+          <TriangleUpIcon />
+          <span>593</span>
+        </button>
+
+        <div>
+          <p>B</p>
+        </div>
+
+        <div>
+          <p>ByteGrad</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+            est expedita libero officia eum distinctio hic quidem, sit saepe,
+            rem quos tempora earum. Porro hic natus nostrum ipsam? Sint, est?
+          </p>
+        </div>
+        <p>4d</p>
+      </li>
+    </ol>
+  );
 };
 
 export default FeedbackList;
