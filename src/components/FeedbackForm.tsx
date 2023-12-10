452b3a6057d20375bad68c83162508c0cbe65bf3
LeonLonsdale <leon.coding@icloud.com>
2023-12-10 14:27:00 +0000

    FeedbackForm component

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,7 +1,24 @@
 type FeedbackFormProps = {};
 
 const FeedbackForm = ({}: FeedbackFormProps) => {
-  return <form>FeedbackForm</form>;
+  return (
+    <form className='form' onSubmit={() => {}}>
+      <textarea
+        id='feedback-textarea'
+        placeholder='placeholder'
+        spellCheck={false}
+      />
+      <label htmlFor='feedback-textarea'>
+        Enter your feedback here. Remember to #hashtag the company.
+      </label>
+      <div>
+        <p className='u-italic'>150</p>
+        <button type='submit' className=''>
+          <span>Submit</span>
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default FeedbackForm;
