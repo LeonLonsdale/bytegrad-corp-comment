a61fdb27b05d089df21852d04c079613cbfb1eb9
LeonLonsdale <leon.coding@icloud.com>
2023-12-12 11:55:40 +0000

    expand feedbacks on click

--- src/components/feedback/FeedbackItem.tsx
+++ src/components/feedback/FeedbackItem.tsx
@@ -1,6 +1,7 @@
 import { TriangleUpIcon } from '@radix-ui/react-icons';
 import { MAX_CHARACTERS } from '../../lib/constants';
 import { TFeedbackItem } from '../../lib/types';
+import { useState } from 'react';
 
 type FeedbackItemProps = {
   feedbackItem: TFeedbackItem;
@@ -8,9 +9,15 @@ type FeedbackItemProps = {
 
 const FeedbackItem = ({ feedbackItem }: FeedbackItemProps) => {
   const capitalLetter = feedbackItem.company.charAt(0).toUpperCase();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => setIsOpen(!isOpen);
 
   return (
-    <li className='feedback'>
+    <li
+      onClick={handleClick}
+      className={`feedback ${isOpen ? 'feedback--expand' : ''}`}
+    >
       <button>
         <TriangleUpIcon />
         <span>{feedbackItem.upvoteCount}</span>
